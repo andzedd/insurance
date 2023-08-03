306eb686f8cb0a6da557bb46d3dd86db76ffd923
Matheus dos Santos Souza <andzedd.matheus@gmail.com>
2023-08-02 21:14:26 -0300

    incendio util

--- src/controllers/incendioController.js
+++ src/controllers/incendioController.js
@@ -1,32 +1,10 @@
 import incendio from "../models/Incendio.js";
+import taxaRisco from "../util/taxaRisco.js";
 
 class IncendioController {
     static taxaRisco = async (req,res) => {
         let string = "idzmaxlimvlr01=7800;eqpmrc=1;eqpmdl=3"
-        const parametros = string.split(';');
-        const filtro = {};
-        let ajuste = 1;
-      
-        for (const param of parametros) {
-            const [chave, valor] = param.split('=');
-            filtro[`${chave}`] = valor;
-        }
-
-        try {
-            const resultado = await incendio.find(filtro);
-      
-            if (!resultado || resultado.length === 0) {
-              return res.status(404).send("Nenhuma taxa de risco encontrada");
-            } else {
-                for(let x in resultado){
-                ajuste = ajuste * parseFloat(resultado[x].ajt)
-                }
-            }
-            res.status(200).send(`Taxa de risco total: ${ajuste}`);
-        } catch (error) {
-            console.error('Erro ao buscar ajuste:', error);
-            res.status(500).send("erro");
-        }
+        taxaRisco(req,res,string,incendio)
     }
 }
 
