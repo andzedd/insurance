0b4f2cc0cf39568209dc6a003e4a7e69cea78dbb
Matheus dos Santos Souza <andzedd.matheus@gmail.com>
2023-08-15 15:33:11 -0300

    Esqueci

--- src/util/taxaRisco.js
+++ src/util/taxaRisco.js
@@ -11,11 +11,12 @@ const taxaRisco = async (req,res,string,cob) => {
     try {
         const resultado = await cob.find(filtro);
         if (!resultado || resultado.length === 0) {
-            return res.status(404).send("Nenhuma taxa de risco encontrada");
+            return res.status(404).send(filtro);
         } else {
             for(let x in resultado){
                 ajuste = ajuste * parseFloat(resultado[x].ajt)
             }
+            res.status(202).send(filtro)
         }
         res.status(200).send(`Taxa de risco total: ${ajuste}`);
         } catch (error) {
