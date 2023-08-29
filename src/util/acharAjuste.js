8c4d05210565896f188b5f6f6193f5dd42c7e7d2
Matheus dos Santos Souza <andzedd.matheus@gmail.com>
2023-08-29 13:30:59 -0300

    Estrutura acharAjuste.js

--- src/util/acharAjuste.js
+++ src/util/acharAjuste.js
@@ -4,7 +4,52 @@ const acharAjuste = async (req,res,camada) => {
         const ajustes = [];
 
         const ajustesBanco = await camada.find({}).exec();
+
+        ajustesBanco.forEach(ajuste => {
+            const jsonBanco = { ...ajuste };
+            delete jsonBanco._doc._id;
+            delete jsonBanco._doc.ajt;
+
+            if(comparaObjetos(jsonEntrada,jsonBanco._doc)){
+                ajustes.push(ajuste);
+            }
+        });
+
+        let ajusteFinal = 1;
+        ajustes.forEach(ajuste => {
+            ajusteFinal *= parseFloat(ajuste.ajt);
+        });
+
+        return ajusteFinal;
     } catch (err) {
         res.status(500).send(`Erro: ${err}`);
     }
+}
+
+function filtrarJson(jsonEntrada,jsonBanco){
+    const jsonFiltrado = {};
+    for(const c in jsonEntrada){
+        if(jsonBanco.hasOwnProperty(c)){
+            jsonFiltrado[c] = jsonEntrada[c];
+        }
+    }
+
+    return jsonFiltrado;
+}
+
+function comparaObjetos(jsonEntrada,jsonBanco){
+    const jsonFiltrado = filtrarJson(jsonEntrada,jsonBanco);
+    const chavesFiltradas = Object.keys(jsonFiltrado);
+    const chavesBanco = Object.keys(jsonBanco);
+
+    if(chavesFiltradas.length !== chavesBanco.length){
+        return false;
+    }
+
+    for(const chave of chavesFiltradas){
+        if(jsonFiltrado[chave] !== jsonBanco[chave]){
+            return false;
+        }
+    }
+    return true;
 }
