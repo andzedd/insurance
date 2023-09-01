cddd8d033ad6ce8bffc9ee385ba863ffbfc720d9
Matheus dos Santos Souza <andzedd.matheus@gmail.com>
2023-09-01 16:32:12 -0300

    premio de risco

--- src/controllers/incendioController.js
+++ src/controllers/incendioController.js
@@ -2,10 +2,14 @@ import taxaRiscoUtil from "../util/taxaRiscoUtil.js";
 
 const premioIncendio = (req,res,cobertura) => {
     let premio = 0;
-    const taxa = taxaRiscoUtil(req,res,cobertura);
+    let taxaRisco = valorTaxaRisco(req,res,cobertura);
     premio = taxa;
 
     return premio;
+}
+
+const valorTaxaRisco = (req,res,cobertura) => {
+    return taxaRiscoUtil(req,res,cobertura);
 }
 
 export default premioIncendio;
